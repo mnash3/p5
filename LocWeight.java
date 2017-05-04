5681a28287e6bd162d6ce699c23eb04eadd1a28f
Aseeel <aseel.beshri@gmail.com>
2017-05-04 18:25:36 -0500

    Update LocWeight.java

--- LocWeight.java
+++ LocWeight.java
@@ -1,3 +1,10 @@
+// Semester:         CS367 Spring 2017 
+// PROJECT:          p5
+// FILE:             LocWeight.java
+//
+// TEAM:    Team 196
+// Author1: (Aseel Albeshri,albeshri@wisc.edu,albeshri,lecture 02)
+// Author2: (Mickey Nash,mnash3@wisc.edu,mnash3,lecture002)
 
 public class LocWeight implements Comparable<LocWeight>{
 	private Location location;
